5b1bc2db008a924df94d6ed94d40e854db7f007c
Andrus Naulainen <andrus.naulainen@gmail.com>
2020-02-19 10:07:02 +0200

    Fetch test

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-const request = new XMLHttpRequest()
-
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-request.onload = function() {
-  // Begin accessing JSON data here
-    let data = JSON.parse(this.response)
-    const container = document.getElementById('container')
-
-    data.forEach(movie => {
-        console.log(movie.description)
-        const movie_div = document.createElement('div')
-        movie_div.innerHTML = movie.title
-        container.append(movie_div)
-    });
-
-
-
-}
-
-// Send request
-request.send()
+fetch('https://pokeapi.co/api/v2/pokemon')
+.then(res => {
+  return res.json()
+})
+.then(data => {
+  console.log(data.results)
+})
